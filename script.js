66b8b62ccee01ec089239c89c4ccb9077a98cc20
omtanpure3008 <162991862+omtanpure3008@users.noreply.github.com>
2024-06-28 13:14:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var kiran = document.querySelector('#KESHAV');
+
+// kiran.innerHTML = "Hello payal";
+// kiran.innerText = "My name is om";
+// kiran.textContent = "My name is vaishnavi";
+
+
+// console.log(kiran.innerHTML);
+
+
+kiran.style.textAlign = "left";
